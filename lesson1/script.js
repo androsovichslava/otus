c67165eff2be706cba1e47839cbc6b6188018e5b
androsovichslava <androsovichslava@gmail.com>
2024-04-11 21:34:58 +0300

    sss

--- lesson1/script.js
+++ lesson1/script.js
@@ -48,6 +48,40 @@ function isFit(circleArea, squareArea) {
   return diameter <= side;
 }
 
+
+
+// Создайте объект user, содержащий поле name со значением ‘John’.
+const user = {
+  name: 'Jhon'
+}
+
+// Запросить у пользователя ввод числа. Записать введенное значение в поле age объекта user.
+let ageInput = prompt();
+user.age = ageInput;
+// debugger
+// Создать копию объекта user с именем admin. Добавить новому объекту поле role со значением ‘admin’.
+const admin = JSON.parse(JSON.stringify(user));
+
+// Записать все значения полей объекта admin в отдельные переменные. Имена переменных должны совпадать с названиями полей
+let name = admin.name;
+let age = admin.age;
+console.log(admin);
+
+// Массивы
+// Создайте массив целых чисел из 10 элементов.
+const sampleArray = [4,55,7,88,98,3,-11,578, -99, 4];
+
+// Выведите в консоль сумму всех элементов массива.
+const sumElements = sampleArray.reduce((a,c)=>a+c,0);
+console.log(sumElements)
+// Создайте новый массив на основе исходного, в котором каждый элемент будет вдвое больше элемента исходного массива с таким же индексом. 
+const doubleElements = sampleArray.map(e=>2 * e)
+console.log('Удвоенный',doubleElements);
+
+// (a[1] = 3, b[1] = 6, где a — исходный массив, b — новый массив).
+
+// Найдите и выведите в консоль наибольший и наименьший элементы исходного массива.
+
 console.log("Произведение a b: " + sumAndMult(3, 60).mul);
 console.log("Сумма a b: " + sumAndMult(3, 60).sum);
 console.log(lengthOfStrings("www", "2"));
